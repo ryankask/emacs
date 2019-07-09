2c173b9f2a37c4925a07ec6de46d24876e8f584c
Phil Sainty <psainty@orcon.net.nz>
2019-07-10 00:22:14 +1200

    fixup! Add so-long library
    Update minor mode and variable override defaults

--- lisp/so-long.el
+++ lisp/so-long.el
@@ -680,6 +680,8 @@ was established."
   '(font-lock-mode ;; (Generally the most important).
     ;; Other standard minor modes:
     display-line-numbers-mode
+    goto-address-mode
+    goto-address-prog-mode
     hi-lock-mode
     highlight-changes-mode
     hl-line-mode
@@ -728,6 +730,7 @@ they are in Emacs core, GNU ELPA, or elsewhere."
     (buffer-read-only . t)
     (global-hl-line-mode . nil)
     (line-move-visual . t)
+    (show-paren-mode . nil)
     (truncate-lines . nil)
     (which-func-mode . nil))
   "Variables to override, and the values to override them with.
